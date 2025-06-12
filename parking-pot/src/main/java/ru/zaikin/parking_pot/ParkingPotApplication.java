d07bcabc4b7e1c1637e0d659d4550c689cbae275
hitoricamiter <koshurnikovaslyam@mail.ru>
2025-06-13 04:32:11 +0600

    add start enviroinment and postgres container

---
+++ parking-pot/src/main/java/ru/zaikin/parking_pot/ParkingPotApplication.java
@@ -0,0 +1,13 @@
+package ru.zaikin.parking_pot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingPotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingPotApplication.class, args);
+	}
+
+}
---
+++ parking-pot/src/test/java/ru/zaikin/parking_pot/ParkingPotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.zaikin.parking_pot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingPotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
